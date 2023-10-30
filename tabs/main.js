c2110d5cd6ab6b35a0216048b632af514e7b6ae0
jannet18 <janetngei18@gmail.com>
2023-10-30 15:49:40 +0300

    final tab project

---
+++ tabs/main.js
@@ -0,0 +1,21 @@
+const btns = document.querySelectorAll('.tab-btn');
+const articles = document.querySelectorAll('.content');
+const about = document.querySelector('.about');
+
+about.addEventListener('click', function (e){
+    const id = e.target.dataset.id;
+    if(id){
+        // remove active class from all btns
+        btns.forEach(function(btn){
+            btn.classList.remove('active');
+        });
+        e.target.classList.add('active');
+        // hide other articles
+            articles.forEach(function(article){
+                article.classList.remove('active');
+            });
+            const element = document.getElementById(id);
+            element.classList.add('active');
+    }
+});
+
