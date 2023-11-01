43a2a043261e93cc6c068c913265f48c78050010
jannet18 <janetngei18@gmail.com>
2023-11-01 13:49:21 +0300

    slider project

---
+++ slider/main.js
@@ -0,0 +1,24 @@
+const slides = document.querySelectorAll('.slider');
+const nxtBtn = document.querySelector('.nextBtn');
+const prevBtn = document.querySelector('.prevBtn');
+
+slides.forEach(function(slide, index){
+    slide.style.left = `${index * 100}%`;
+});
+
+let counter = 0;
+nxtBtn.addEventListener('click', function(){
+    counter++;
+    carousel();
+});
+
+prevBtn.addEventListener('click', function () {
+    counter--;
+    carousel();
+});
+
+function carousel () {
+slides.forEach(function(slide) {
+    slide.style.transform = `translateX-(${counter * 100}%)`
+});
+};
