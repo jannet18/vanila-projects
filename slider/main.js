66763a3839973f046f0133197f17197eb936d499
jannet18 <janetngei18@gmail.com>
2023-11-02 16:56:47 +0300

    slider carousel

--- slider/main.js
+++ slider/main.js
@@ -1,4 +1,4 @@
-const slides = document.querySelectorAll('.slider');
+const slides = document.querySelectorAll('.slide');
 const nxtBtn = document.querySelector('.nextBtn');
 const prevBtn = document.querySelector('.prevBtn');
 
@@ -18,7 +18,29 @@ prevBtn.addEventListener('click', function () {
 });
 
 function carousel () {
+    // working with slides
+    if (counter === slides.length) {
+        counter = 0;
+    }
+    if (counter < 0) {
+        counter = slides.length - 1;
+    }
+
+    // working with buttons
+    if (counter === slides.length - 1) {
+        nxtBtn.style.display = 'block';
+    } else {
+        nxtBtn.style.display = 'none';
+    }
+
+    if (counter > 0){
+        prevBtn.style.display = 'block';
+    } else {
+        prevBtn.style.display = 'none';
+    }
+
 slides.forEach(function(slide) {
-    slide.style.transform = `translateX-(${counter * 100}%)`
+    slide.style.transform = `translateX(-${counter * 100}%)`;
 });
 };
+prevBtn.style.display = 'none';
