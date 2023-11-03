cb69666775ff92bd5cabfe349ca95376d0fb9ebf
jannet18 <janetngei18@gmail.com>
2023-11-03 15:27:38 +0300

    feeback ui

---
+++ feeback-ui/main.js
@@ -0,0 +1,33 @@
+const ratingEls = document.querySelectorAll('.ratings-container');
+const btnEl = document.getElementById('btn');
+
+const container = document.querySelector('.container');
+
+let selectedRating = '';
+function removeActive(){
+    ratingEls.forEach(function (ratingEl){
+        ratingEl.classList.remove('active');
+    })
+}
+
+ratingEls.forEach((ratingEl) => {
+    ratingEl.addEventListener('click', function (e){
+        removeActive();
+        selectedRating = e.target.innerText || e.target.parentNode.innerText;
+        e.target.classList.add('active');
+        e.target.parentNode.classList.add("active");
+    });
+});
+
+
+btnEl.addEventListener('click', function () {
+    if (selectedRating !== ''){
+container.innerHTML = `<strong>Thank you!</strong>
+<br>
+<br>
+<strong>Feedback: ${selectedRating}</strong>
+<p>We'll use your feedback to improve our customer support.</p>
+`;
+    }
+});
+
